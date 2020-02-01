8250a41d80ee11e43146ca9b01eee63245fb2a19
Stralle <strahinja.rodic@hotmail.com>
2020-02-01 22:55:01 +0100

    Added buzzsaw prefab

---
+++ Assets/Scripts/CollectableItems/BuzzSaw.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzSaw : Trap
+{
+    [SerializeField]
+    Sprite _brokenBuzzSawSprite = null;
+    [SerializeField]
+    Sprite _activeBuzzSawSprite = null;
+
+}
