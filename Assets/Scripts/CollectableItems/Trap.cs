8f2c47116590a14914bcd9cb0ae998f260eefece
Stralle <strahinja.rodic@hotmail.com>
2020-02-01 16:42:22 +0100

    Added Trap.cs, IRepairable.cs, Knight sprites is now in tiles

---
+++ Assets/Scripts/CollectableItems/Trap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ETrapType
+{
+    Spikes,
+    Invalid
+}
+
+public class Trap : MonoBehaviour, IRepairable
+{
+    [SerializeField] ETrapType _trapType = ETrapType.Invalid;
+
+    [SerializeField] Animator _animator = null;
+
+    bool _isRepaired = false;
+
+    bool _isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public ETrapType GetTrapType()
+    {
+        return _trapType;
+    }
+
+    public void RepairAndSpendResources()
+    {
+        // TODO: Implement logic here and stuff
+        throw new System.NotImplementedException();
+    }
+}
