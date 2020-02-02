7e275bbffa5df8be1a416b91f7941fd251f36da9
Daniil Privalov <iconbitpro@gmail.com>
2020-02-02 15:25:06 +0100

    Go to next level - go to results

--- Assets/Scripts/Enemy/WinAnimationBehavior.cs
+++ Assets/Scripts/Enemy/WinAnimationBehavior.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeadAnimationBehaviour : StateMachineBehaviour
+public class WinAnimationBehavior : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	//{
+	//    
+	//}
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	//{
+	//    
+	//}
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		GameRulesManager.Instance.GoToNextLevel();
+		GameRulesManager.Instance.EndGame();
 	}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
