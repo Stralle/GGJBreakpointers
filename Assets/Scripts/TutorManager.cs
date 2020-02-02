365fa0585e83364d5a65468960f4c0d0b10471f3
Daniil Privalov <iconbitpro@gmail.com>
2020-02-02 15:51:28 +0100

    Tutorial support

---
+++ Assets/Scripts/TutorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Video;
+
+public class TutorManager : MonoBehaviour
+{
+	VideoPlayer vp = null;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		vp = GetComponent<VideoPlayer>();
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			SceneLoader.LoadScene(SceneLoader.Scenes.MainMenu);
+		}
+
+		//if (!vp.isPlaying)
+		//{
+		//	SceneLoader.LoadScene(SceneLoader.Scenes.MainMenu);
+		//}
+	}
+}
