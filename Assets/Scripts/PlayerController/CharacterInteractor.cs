2a471066a27c387a596291dfeb46951627b2563b
Aleksandar Zdravković <pajgla@users.noreply.github.com>
2020-02-02 09:58:05 +0100

    Adding Character Interactor and changing its trigger radius

---
+++ Assets/Scripts/PlayerController/CharacterInteractor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteractor : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        InteractablePart _isResource = collision.GetComponent<InteractablePart>();
+        if (_isResource)
+        {
+            IDestructible destructible = _isResource.GetMainGameObject().GetComponent<Loot>() as IDestructible;
+            if (destructible != null)
+            {
+                destructible.DestroyAndGetResources();
+            }
+        }
+    }
+}
