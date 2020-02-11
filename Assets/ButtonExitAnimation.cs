dc48f0101c47d10593a1e2729ac080d9fe6dd954
DmitraLunaris <60327941+DmitraLunaris@users.noreply.github.com>
2020-02-11 23:09:36 +0100

    Added exit game button and anim.

---
+++ Assets/ButtonExitAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonExitAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    public void ButtonExitGame(){
+        GetComponent<ButtonExitAnimation>().Play("ButtonExitGame")
+    }
+}
